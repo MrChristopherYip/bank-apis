94e2656459d0f269b0e56164f4531579788fff02
MrChristopherYip <47013268+MrChristopherYip@users.noreply.github.com>
2021-03-05 03:34:32 -0800

    Initial commit of project files pre api conversion

---
+++ src/main/java/com/flexon/springboot/bankapis/BankApisApplication.java
@@ -0,0 +1,13 @@
+package com.flexon.springboot.bankapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flexon/springboot/bankapis/BankApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexon.springboot.bankapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
